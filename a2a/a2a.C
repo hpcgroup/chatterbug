670adf3c111b7c7d247f54e26a2ecbbfddcac03a
Nikhil Jain <nikhil.jain@acm.org>
2015-08-03 10:21:52 -0500

    Two-dimensional A2A
    Change-Id: I193004bd2f8133c1e9bf2a661f937ddf09ef2681

--- a2a/a2a.C
+++ a2a/a2a.C
@@ -8,18 +8,22 @@
 
 #define MAX_ITER 10
 
+#define calc_pe(a,b,c)  ((a)+(b)*dims[MP_X]+(c)*dims[MP_X]*dims[MP_Y])
+
 int main(int argc, char **argv)
 {
   MPI_Init(&argc,&argv);
 
-  int i,myrank, numranks, groupsize;
-  int dims[3] = {0, 0, 0};          
-  int temp[3] = {0, 0, 0};          
-  int coord[3] = {0, 0, 0};          
-  int periods[3] = {1, 1, 1};
-  double startTime, start5Time, stopTime;
+  if(argc != 5) {
+    printf("Correct usage: ./%s dimX dimY dimZ bytes_per_pair", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 0);
+  }
 
-  MPI_Comm cartcomm, subcomm;
+  int i, myrank, numranks, off;
+  int dims[3] = {0, 0, 0};
+  double startTime, stopTime;
+
+  MPI_Request *sreq, *rreq;
 
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
   MPI_Comm_size(MPI_COMM_WORLD,&numranks);
@@ -27,40 +31,66 @@ int main(int argc, char **argv)
   dims[MP_X] = atoi(argv[1]);
   dims[MP_Y] = atoi(argv[2]);
   dims[MP_Z] = atoi(argv[3]);
-  MPI_Dims_create(numranks, 3, dims);
-  MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 1, &cartcomm);
-  MPI_Cart_get(cartcomm, 3, dims, periods, coord);
-  temp[MP_X] = 0; temp[MP_Y] = 1; temp[MP_Z] = 0;
-  MPI_Cart_sub(cartcomm, temp, &subcomm);
-
-  MPI_Comm_size(subcomm,&groupsize);
   int perrank = atoi(argv[4]);
-  char *sendbuf = (char*)malloc(perrank*groupsize);
-  char *recvbuf = (char*)malloc(perrank*groupsize);
 
-  MPI_Barrier(cartcomm);
+  int largerGroup = (dims[MP_X] > dims[MP_Y]) ? dims[MP_X] : dims[MP_Y];
+
+  char *sendbuf = (char*)malloc(perrank * largerGroup);
+  char *recvbuf = (char*)malloc(perrank * largerGroup);
+  sreq = new MPI_Request[largerGroup];
+  rreq = new MPI_Request[largerGroup];
+
+  int myXcoord = myrank % dims[MP_X];
+  int myYcoord = (myrank % (dims[MP_X] * dims[MP_Y])) / dims[MP_X];
+  int myZcoord = (myrank % (dims[MP_X] * dims[MP_Y] * dims[MP_Z])) / (dims[MP_X] * dims[MP_Y]);
+
+#if CMK_BIGSIM_CHARM
+  AMPI_Set_startevent(MPI_COMM_WORLD);
+#endif
   startTime = MPI_Wtime();
-  for (i=0; i<MAX_ITER; i++) {
+  for (i = 0; i < MAX_ITER; i++) {
 #if CMK_BIGSIM_CHARM
     if(!myrank)
       BgPrintf("Current time is %f\n");
+#else
+    if(!myrank)
+      printf("Current time is %f\n", MPI_Wtime());
 #endif
-    if(i == 3) {
-      MPI_Barrier(MPI_COMM_WORLD);
-      start5Time = MPI_Wtime();
+    off = 0;
+    for(int ycoord = 1; ycoord < dims[MP_Y]; ycoord++) {
+      MPI_Isend(&sendbuf[off], perrank, MPI_CHAR,
+        calc_pe(myXcoord, (ycoord + myYcoord) % dims[MP_Y], myZcoord), 0,
+        MPI_COMM_WORLD, &sreq[ycoord]);
+      MPI_Irecv(&recvbuf[off], perrank, MPI_CHAR,
+        calc_pe(myXcoord, (ycoord + myYcoord) % dims[MP_Y], myZcoord), 0,
+        MPI_COMM_WORLD, &rreq[ycoord]);
+      off += perrank;
     }
-    MPI_Alltoall(sendbuf, perrank, MPI_CHAR, recvbuf, perrank, MPI_CHAR, subcomm);
+    MPI_Waitall(dims[MP_Y] - 1, &sreq[1], MPI_STATUSES_IGNORE);
+    MPI_Waitall(dims[MP_Y] - 1, &rreq[1], MPI_STATUSES_IGNORE);
+    off = 0;
+    for(int xcoord = 1; xcoord < dims[MP_X]; xcoord++) {
+      MPI_Isend(&sendbuf[off], perrank, MPI_CHAR,
+        calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord, myZcoord), 0,
+        MPI_COMM_WORLD, &sreq[xcoord]);
+      MPI_Isend(&recvbuf[off], perrank, MPI_CHAR,
+        calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord, myZcoord), 0,
+        MPI_COMM_WORLD, &rreq[xcoord]);
+      off += perrank;
+    }
+    MPI_Waitall(dims[MP_X] - 1, &sreq[1], MPI_STATUSES_IGNORE);
+    MPI_Waitall(dims[MP_X] - 1, &rreq[1], MPI_STATUSES_IGNORE);
   }
-  MPI_Barrier(cartcomm);
   stopTime = MPI_Wtime();
 #if CMK_BIGSIM_CHARM
   if(!myrank)
-    BgPrintf("After barrier Current time is %f\n");
+    BgPrintf("After loop Current time is %f\n");
 #endif
 
   if(myrank == 0) {
     printf("Finished %d iterations\n",MAX_ITER);
-    printf("Time elapsed per iteration for size %d: %f\n", perrank, (stopTime - start5Time)/(MAX_ITER-3));
+    printf("Time elapsed per iteration for size %d: %f\n", perrank, (stopTime -
+    startTime)/MAX_ITER);
   }
   MPI_Finalize();
 }
