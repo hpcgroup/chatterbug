caeff39a05619ec4d4fab03c837cb750ad0db686
Nikhil Jain <nikhil.jain@acm.org>
2015-08-03 13:51:09 -0500

    Clean up ping example
    Change-Id: I0fb7f9a58e6f1d57e01ab63d16604e1b5e7cc71c

--- ping/ping.C
+++ ping/ping.C
@@ -4,84 +4,59 @@
 
 int main(int argc, char **argv)
 {
-  MPI_Init(&argc,&argv);
   int rank, numranks;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
-  double starttime, endtime;
-  MPI_Status status;
+  if(!rank && argc != 5) {
+    printf("Correct usage: ./%s src dst bytes num_iter\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 0);
+  }
 
   int src = atoi(argv[1]);
   int dst = atoi(argv[2]);
   int size = atoi(argv[3]);
   int numIter = atoi(argv[4]);
 
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&numranks);
-
+  double starttime, endtime;
   char *question;
 
   if(src == rank || dst == rank) {
     question = (char*)malloc(size*sizeof(char));
   }
 
-  //warm up
-  for(int i = 0; i < 10; i++) {
-    if(rank == src) {
-      MPI_Send(question,size,MPI_CHAR,dst,0,MPI_COMM_WORLD);
-    } else if(rank == dst) {
-      MPI_Recv(question,size,MPI_CHAR,src,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-    }
-  }
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  starttime = MPI_Wtime();
 #if CMK_BIGSIM_CHARM
-    if(!rank)
-      BgPrintf("Before iteration Current time is %f\n");
+  AMPI_Set_startevent(MPI_COMM_WORLD);
 #endif
+  starttime = MPI_Wtime();
   for(int i = 0; i < numIter; i++) {
 #if CMK_BIGSIM_CHARM
     if(!rank)
       BgPrintf("Current time is %f\n");
+#else
+    if(!rank)
+      printf("Current time is %f\n", MPI_Wtime());
 #endif
     if(rank == src) {
-      MPI_Send(question,size,MPI_CHAR,dst,0,MPI_COMM_WORLD);
+      MPI_Send(question, size, MPI_CHAR, dst, 0, MPI_COMM_WORLD);
     } else if(rank == dst) {
-      MPI_Recv(question,size,MPI_CHAR,src,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      MPI_Recv(question, size, MPI_CHAR, src, 0, MPI_COMM_WORLD,
+        MPI_STATUS_IGNORE);
     }
   }
   endtime = MPI_Wtime();
-  MPI_Barrier(MPI_COMM_WORLD);
 #if CMK_BIGSIM_CHARM
   if(!rank)
-    BgPrintf("After barrier Current time is %f\n");
+    BgPrintf("After loop Current time is %f\n");
 #endif
-  
+
   if(rank == dst)
-    printf("[%d] Time for %d size %lf: (%lf %lf)\n",rank,size,(endtime-starttime)/numIter,endtime,starttime);
-  
-  MPI_Barrier(MPI_COMM_WORLD);
-#if CMK_BIGSIM_CHARM
-  if(!rank)
-    BgPrintf("Current time is %f\n");
-#endif
-  starttime = MPI_Wtime();
-  if(rank == src) {
-    MPI_Send(question,size,MPI_CHAR,dst,0,MPI_COMM_WORLD);
-  } else if(rank == dst) {
-    MPI_Recv(question,size,MPI_CHAR,src,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-  }
-  endtime = MPI_Wtime();
-#if CMK_BIGSIM_CHARM
-  if(!rank)
-    BgPrintf("Current time is %f\n");
-#endif
-  
-  if(rank == dst || rank == src)
-    printf("[%d] Time for %d size %lf: (%lf %lf)\n",rank,size,(endtime-starttime),endtime,starttime);
+    printf("[%d] Time for size %d is %lf (%lf %lf)\n", rank, size,
+      (endtime-starttime)/numIter, endtime, starttime);
 
   MPI_Finalize();
 }
 
-  
-      
+
+
