2b5bb88a594c1dd49bcd841b89ef4241b42d0d2b
Abhinav Bhatele <bhatele@llnl.gov>
2017-03-14 14:44:18 -0700

    qbox: partial set of changes to enable OTF2 support

--- qbox/qbox_mini.C
+++ qbox/qbox_mini.C
@@ -5,6 +5,9 @@
 #define MP_X 0
 #define MP_Y 1
 
+#if WRITE_OTF2_TRACE
+#include <scorep/SCOREP_User.h>
+#elif CMK_BIGSIM_CHARM
 #include "shared-alloc.h"
 #include "blue.h"
 #include "cktiming.h"     
@@ -15,6 +18,7 @@ void changeMessage(BgTimeLog *log, int size)
 }
 
 extern "C" void BgMark(const char *str);
+#endif
 
 #define calc_pe(a,b)  ((a)+(b)*dims[MP_X])
 
@@ -27,24 +31,32 @@ void All2All(void *sendbuf, void *recvbuf, int size, int myrank, int *dims, int 
   int myXcoord = myrank % dims[MP_X];
   int myYcoord = myrank / dims[MP_X];
 
-  BgTimeLine &timeLine = tTIMELINEREC.timeline;  
+#if CMK_BIGSIM_CHARM
+  BgTimeLine &timeLine = tTIMELINEREC.timeline;
+#endif
   for(int xcoord = 1; xcoord < dims[MP_X]; xcoord++) {
     MPI_Isend(sendbuf, 1, MPI_CHAR,
         calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord), 0,
         MPI_COMM_WORLD, &sreq[xcoord]);
+#if CMK_BIGSIM_CHARM
     changeMessage(timeLine[timeLine.length() - 3], size);
+#endif
     MPI_Irecv(recvbuf, 1, MPI_CHAR,
         calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord), 0,
         MPI_COMM_WORLD, &rreq[xcoord]);
   }
+#if CMK_BIGSIM_CHARM
   if(overlap) {
     BgMark("QBOX_OverlapWork");    
   }
+#endif
   MPI_Waitall(dims[MP_X] - 1, &sreq[1], MPI_STATUSES_IGNORE);
   MPI_Waitall(dims[MP_X] - 1, &rreq[1], MPI_STATUSES_IGNORE);
+#if CMK_BIGSIM_CHARM
   if(!overlap) {
     BgMark("QBOX_FFTTime");    
   }
+#endif
 }
 
 void Reduce(void *sendbuf, void *recvbuf, int size, int myrank, int *dims, int alongDim) {
@@ -59,7 +71,9 @@ void Reduce(void *sendbuf, void *recvbuf, int size, int myrank, int *dims, int a
   int myChild1 = 2*myRank + 1;
   int myChild2 = myChild1 + 1;
 
-  BgTimeLine &timeLine = tTIMELINEREC.timeline;  
+#if CMK_BIGSIM_CHARM
+  BgTimeLine &timeLine = tTIMELINEREC.timeline;
+#endif
   int partCoord[2];
   partCoord[otherDim] = mycoord[otherDim];
   if(myChild1 < commSize) {
@@ -75,7 +89,9 @@ void Reduce(void *sendbuf, void *recvbuf, int size, int myrank, int *dims, int a
   if(myRank != 0) {
     partCoord[alongDim] = myParent;
     MPI_Send(sendbuf, 1, MPI_CHAR,  calc_pe(partCoord[MP_X], partCoord[MP_Y]), 0, MPI_COMM_WORLD);
+#if CMK_BIGSIM_CHARM
     changeMessage(timeLine[timeLine.length() - 3], size);
+#endif
   }
 }
 
@@ -91,7 +107,9 @@ void Bcast(void *sendbuf, int size, int myrank, int *dims, int alongDim) {
   int myChild1 = 2*myRank + 1;
   int myChild2 = myChild1 + 1;
 
-  BgTimeLine &timeLine = tTIMELINEREC.timeline;  
+#if CMK_BIGSIM_CHARM
+  BgTimeLine &timeLine = tTIMELINEREC.timeline;
+#endif
   int partCoord[2];
   partCoord[otherDim] = mycoord[otherDim];
   if(myRank != 0) {
@@ -105,13 +123,17 @@ void Bcast(void *sendbuf, int size, int myrank, int *dims, int alongDim) {
     partCoord[alongDim] = myChild1;
     MPI_Isend(sendbuf, 1, MPI_CHAR, calc_pe(partCoord[MP_X], partCoord[MP_Y]), 0,
       MPI_COMM_WORLD, &requests[count++]);
+#if CMK_BIGSIM_CHARM
     changeMessage(timeLine[timeLine.length() - 3], size);
+#endif
   }
   if(myChild2 < commSize) {
     partCoord[alongDim] = myChild2;
     MPI_Isend(sendbuf, 1, MPI_CHAR, calc_pe(partCoord[MP_X], partCoord[MP_Y]), 0,
       MPI_COMM_WORLD, &requests[count++]);
+#if CMK_BIGSIM_CHARM
     changeMessage(timeLine[timeLine.length() - 3], size);
+#endif
   }
   MPI_Waitall(count, &requests[0], MPI_STATUSES_IGNORE);
 }
@@ -120,9 +142,13 @@ int main(int argc, char **argv)
 {
   int i, myrank, numranks, off;
   MPI_Init(&argc,&argv);
+#if WRITE_OTF2_TRACE
+  SCOREP_RECORDING_OFF();
+#elif CMK_BIGSIM_CHARM
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Set_trace_status(0);
   MPI_Barrier(MPI_COMM_WORLD);
+#endif
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
   MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
@@ -139,18 +165,36 @@ int main(int argc, char **argv)
   int bcast_size = atoi(argv[4]);
   int allreduce_size = atoi(argv[5]);
   int MAX_ITER = atoi(argv[6]);
+  int max_msg_size = 1;
 
-  char *sendbuf = (char*)shalloc(1,  1);
-  char *recvbuf = (char*)shalloc(1, 1);
-  
+  double startTime, stopTime;
+  char *sendbuf, *recvbuf;
+
+#if CMK_BIGSIM_CHARM
+  sendbuf = (char*) shalloc(1, 1);
+  recvbuf = (char*) shalloc(1, 1);
+#else
+  sendbuf = (char*) malloc (max_msg_size * sizeof(char));
+  recvbuf = (char*) malloc (max_msg_size * sizeof(char));
+#endif
+
   MPI_Barrier(MPI_COMM_WORLD);
+#if WRITE_OTF2_TRACE
+  SCOREP_RECORDING_ON();
+  SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_Loop", SCOREP_USER_REGION_TYPE_COMMON);
+  if(!myrank)
+    SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_WallTime_Loop", SCOREP_USER_REGION_TYPE_COMMON);
+#elif CMK_BIGSIM_CHARM
   MPI_Set_trace_status(1);
   AMPI_Set_startevent(MPI_COMM_WORLD);
-
+  BgTimeLine &timeLine = tTIMELINEREC.timeline;
   if(!myrank)
     BgPrintf("Current time is %f\n");
+#endif
+  startTime = MPI_Wtime();
 
   for (i = 0; i < MAX_ITER; i++) {
+#if CMK_BIGSIM_CHARM
     BgMark("QBOX_Setup");    
     All2All(sendbuf, recvbuf, fft_size, myrank, dims, 0);
     Reduce(sendbuf, recvbuf, allreduce_size, myrank, dims, MP_X);
@@ -159,23 +203,34 @@ int main(int argc, char **argv)
     All2All(sendbuf, recvbuf, fft_size, myrank, dims, 0);
     BgMark("QBOX_Work2");    
     MPI_Loop_to_start();
+#else
+#endif
   }
 
+#if WRITE_OTF2_TRACE
+  SCOREP_USER_REGION_BY_NAME_END("TRACER_Loop");
+#elif CMK_BIGSIM_CHARM
   AMPI_Set_endevent();
+#endif
+
   MPI_Barrier(MPI_COMM_WORLD);
-#if CMK_BIGSIM_CHARM
+  stopTime = MPI_Wtime();
+
+  if(myrank == 0 && MAX_ITER != 0) {
+    printf("Finished %d iterations\n", MAX_ITER);
+    printf("Time elapsed per iteration: %f\n", (stopTime - startTime)/MAX_ITER);
+  }
+
+#if WRITE_OTF2_TRACE
+  if(!myrank)
+    SCOREP_USER_REGION_BY_NAME_END("TRACER_WallTime_Loop");
+  SCOREP_RECORDING_OFF();
+#elif CMK_BIGSIM_CHARM
   if(!myrank)
     BgPrintf("After loop Current time is %f\n");
-#endif
   MPI_Set_trace_status(0);
-
+  MPI_Barrier(MPI_COMM_WORLD);
+#endif
   MPI_Finalize();
 }
-
-
-
-
-
-
-
 
