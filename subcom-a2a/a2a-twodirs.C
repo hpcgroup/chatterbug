7366cedb8abeb7938d042a3beb2b66181c5aab10
Abhinav Bhatele <bhatele@llnl.gov>
2017-03-15 09:35:14 -0700

    subcom-a2a: fix Makefile and compile errors

--- subcom-a2a/a2a-twodirs.C
+++ subcom-a2a/a2a-twodirs.C
@@ -56,11 +56,11 @@ int main(int argc, char **argv)
   int largerMsg = (perrank_x*dims[MP_X] > perrank_y*dims[MP_Y]) ? perrank_x*dims[MP_X] : perrank_y*dims[MP_Y];
 
 #if CMK_BIGSIM_CHARM
-  char *sendbuf = (char*)shalloc(perrank * largerMsg, 1);
-  char *recvbuf = (char*)shalloc(perrank * largerMsg, 1);
+  char *sendbuf = (char*)shalloc(largerMsg, 1);
+  char *recvbuf = (char*)shalloc(largerMsg, 1);
 #else
-  char *sendbuf = (char*)malloc(perrank * largerMsg);
-  char *recvbuf = (char*)malloc(perrank * largerMsg);
+  char *sendbuf = (char*)malloc(largerMsg);
+  char *recvbuf = (char*)malloc(largerMsg);
 #endif
 
   sreq = new MPI_Request[largerGroup];
@@ -93,11 +93,11 @@ int main(int argc, char **argv)
 #if CMK_BIGSIM_CHARM
     off = 0;
     for(int ycoord = 1; ycoord < dims[MP_Y]; ycoord++) {
-      MPI_Isend(&sendbuf[off], perrank, MPI_CHAR,
+      MPI_Isend(&sendbuf[off], perrank_y, MPI_CHAR,
         calc_pe(myXcoord, (ycoord + myYcoord) % dims[MP_Y], myZcoord), 0,
         MPI_COMM_WORLD, &sreq[ycoord]);
       changeMessage(timeLine[timeLine.length() - 3]);
-      MPI_Irecv(&recvbuf[off], perrank, MPI_CHAR,
+      MPI_Irecv(&recvbuf[off], perrank_y, MPI_CHAR,
         calc_pe(myXcoord, (ycoord + myYcoord) % dims[MP_Y], myZcoord), 0,
         MPI_COMM_WORLD, &rreq[ycoord]);
       off += perrank;
@@ -107,11 +107,11 @@ int main(int argc, char **argv)
     MPI_Waitall(dims[MP_Y] - 1, &rreq[1], MPI_STATUSES_IGNORE);
     off = 0;
     for(int xcoord = 1; xcoord < dims[MP_X]; xcoord++) {
-      MPI_Isend(&sendbuf[off], perrank, MPI_CHAR,
+      MPI_Isend(&sendbuf[off], perrank_x, MPI_CHAR,
         calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord, myZcoord), 0,
         MPI_COMM_WORLD, &sreq[xcoord]);
       changeMessage(timeLine[timeLine.length() - 3]);
-      MPI_Irecv(&recvbuf[off], perrank, MPI_CHAR,
+      MPI_Irecv(&recvbuf[off], perrank_x, MPI_CHAR,
         calc_pe((xcoord + myXcoord) % dims[MP_X], myYcoord, myZcoord), 0,
         MPI_COMM_WORLD, &rreq[xcoord]);
       off += perrank;
@@ -136,8 +136,7 @@ int main(int argc, char **argv)
 
   if(myrank == 0 && MAX_ITER != 0) {
     printf("Finished %d iterations\n",MAX_ITER);
-    printf("Time elapsed per iteration for size %d: %f\n", perrank, (stopTime -
-    startTime)/MAX_ITER);
+    printf("Time elapsed per iteration for sizes %d %d: %f\n", perrank_x, perrank_y, (stopTime - startTime)/MAX_ITER);
   }
 
 #if WRITE_OTF2_TRACE
